f6675c8d0e283148363fec12ec473f919ec83215
Lohen <bry.carbonnier@gmail.com>
2019-12-09 14:04:22 +0100

    mise en place du projet + début page accueil

---
+++ src/Controller/AccueilController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccueilController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index()
+    {
+        return $this->render('site/home.html.twig', [
+            'controller_name' => 'AccueilController',
+        ]);
+    }
+
+
+
+
+}
